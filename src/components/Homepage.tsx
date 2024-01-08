f442e977842ecfd859a7df447f4df7de6cbeda89
tbriggs1 <tombriggs1993@outlook.com>
2024-01-08 16:40:35 +0000

    fixed navbar styling issues

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+
 
 const Homepage: React.FC = () => {
     return(
         <div className="container mt-5">
-            <h1>Welcome to my little space on the web</h1>
-            <p>Hello! My name is Tom Briggs, I'm currently working as a DevOps engineer with a passion for automatation!</p>
+            <h1>Hey! I'm Tom Briggs!</h1>
+            <p>I've been working with companies to help auotomate, migrate, test, cloud enviornments</p>
         </div>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Navbar as BootstrapNavbar } from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
+import { Navbar as BootstrapNavbar, Container, Nav } from 'react-bootstrap';
 
 const Navbar: React.FC = () => {
   return (
     <BootstrapNavbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <BootstrapNavbar.Brand href="#home">Tom Briggs</BootstrapNavbar.Brand>
-        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
-        <BootstrapNavbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </BootstrapNavbar.Collapse>
+        <div className="d-flex align-items-center">
+          <BootstrapNavbar.Brand href="#home">Tom Briggs</BootstrapNavbar.Brand>
+        </div>
+        <div className="ml-auto">
+          <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+          <BootstrapNavbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#services">My Services</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#blogs">Blogs</Nav.Link>
+              <Nav.Link href="#contact" style={{ fontWeight: 'bold', color: 'red' }}>
+                Contact Me
+              </Nav.Link>
+            </Nav>
+          </BootstrapNavbar.Collapse>
+        </div>
       </Container>
     </BootstrapNavbar>
   );
