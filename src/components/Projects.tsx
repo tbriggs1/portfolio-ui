e46ea67a1fcb31b79d83add26760b8fa19cbb554
tbriggs1 <tombriggs1993@outlook.com>
2024-01-16 20:36:41 +0000

    added project part

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,42 +1,36 @@
 import React from "react";
-import Carousel from 'react-bootstrap/Carousel';
-import blue from '../images/blue.jpg';
-
-const imageStyle = {
-    width: "500px", // Adjust the width as needed
-    height: "300px", // Adjust the height as needed
-  };
-  
+import Carousel from "react-bootstrap/Carousel";
+import blue from "../images/blue.jpg";
 
 const Projects: React.FC = () => {
-    return (
-        <div className="projects">
-            <h1>Projects</h1>
-            <Carousel>
-            <Carousel.Item>
-                <img src={blue} alt="First Slide" style={imageStyle}/>
-                <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={blue} alt="Second Slide" style={imageStyle}/>
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img src={blue} alt="Third Slide" style={imageStyle}/>
-                <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            </Carousel>
-        </div>
-    );
-}
+  return (
+    <div className="project-main" style={{ width: "100%", margin: "auto" }}>
+      <Carousel>
+        <Carousel.Item style={{ height: "300px" }}>
+          <div className="project-box">
+            <div className="project-text">
+              <h3>Terraform</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+            <div className="project-image">
+              <img src={blue} />
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item style={{ height: "300px" }}>
+          <div className="project-box">
+            <div className="project-text">
+              <h3>Jenkins</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </div>
+            <div className="project-image">
+              <img src={blue} />
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
 
 export default Projects;
